26762c328df7692da3f484e838771408b6a4dd4b
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2024-12-26 21:17:35 +0400

    Prefinal

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const hamburger = document.querySelector('#mobile img');
-    const menu = document.querySelector('#mobile img');
-    
-    // Переключаем класс 'open' у гамбургера
-    hamburger.classList.toggle('opened');
-    menu.classList.toggle('opened');
-}
